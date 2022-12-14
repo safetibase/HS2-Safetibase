1199e1ba1a0179bfbd1c3b0b14a72f089ea8a216
safetibase <49194520+safetibase@users.noreply.github.com>
2022-12-14 14:48:32 +0000

    Add files via upload

---
+++ Manual Install.ps1
@@ -0,0 +1,218 @@
+﻿########################################
+#
+# SafetIbase Installer - Simple version
+#
+########################################
+
+#---------------------------------------
+# INSTRUCTIONS - READ BEFORE RUNNING SCRIPT
+#---------------------------------------
+
+# 1) Using Windows 10 is recommended
+#  - If not running Windows 10: install the PowerShell Gallery. Refer to: https://docs.microsoft.com/en-gb/powershell/gallery/overview
+# 2) Authorised scripts to be run by PowerShell by changing the 'Execution policy' to remote signed. Refer to https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy?view=powershell-6
+# 3) Open as Powershell as administrator
+# 4) Complete the 'User input' section of this script
+# 5) Fill in the excel spreadsheet 'SafetIbaseListsContent - NoUserInput.xlsx' (included in with the install files) for your project.
+# 5) Go to 'Site Contents' > 'Site Assets' and drag and drop the folders contained in the 'Site Asset' folder of the install files.
+# 7) Run the script in one go (f5)
+# 6) once done, your SafetIbase homepage will be available under 'Site Contents' > 'Site Assets' Library > 'pages' > '3.0' > 'dashboard.aspx'
+
+#---------------------------------------
+#  Un-comment next line and run this for those who haven't used PowerShell before
+#---------------------------------------
+
+#set-executionpolicy remotesigned -Scope CurrentUser
+
+#---------------------------------------
+# INPUTS
+#---------------------------------------
+
+#---------------------------------------
+# User inputs
+#---------------------------------------
+
+# enter url to SharePoint site where SafetIbase is to be deployed.
+
+$SPsite = "https://teanent.sharepoint.com/teams/pj-b1234"
+
+#---------------------------------------
+# Fixed inputs
+#---------------------------------------
+
+# If running script by section update the paths to full paths.
+
+$TemplatePath = "$($PSScriptRoot)\List Templates\"
+
+$ExcelListPath = "$($PSScriptRoot)\SafetIbaseListsContent - NoUserInput.xlsx"
+
+
+#---------------------------------------
+# SET UP
+#---------------------------------------
+
+#---------------------------------------
+# Import required functions
+#---------------------------------------
+
+Import-Module "$($PSScriptRoot)\SafetIbaseFunctions.psm1"
+
+<# Used functions
+#Install-RequiredModules - Check is ImportExcel and SharePointPnP modules are installed
+#Convert object to hashtable
+ConvertTo-HashtableFromPsCustomObject
+#Do a topological sort
+Get-TopologicalSort
+#Function required by topological sort function
+Get-ClonedObject
+#>
+
+#---------------------------------------
+# Install required modules
+#---------------------------------------
+
+Install-RequiredModules
+
+#---------------------------------------
+# Connect to SharePoint site
+#---------------------------------------
+
+Try{
+    $connect = Connect-PnPOnline -Url $SPsite -UseWebLogin -ReturnConnection -ErrorAction Stop
+} Catch{
+    $connect = Connect-PnPOnline -Url $SPsite -ReturnConnection
+}
+
+#---------------------------------------
+# CREATE LISTS (USING THE PROVISIONING TEMPLATE)
+#---------------------------------------
+
+#---------------------------------------
+# Sort lists based on lookup dependencies
+#---------------------------------------
+
+#Get hashtable with all lists and their dependencies
+$Regex = [Regex]::new("(?<={listid:)(.*)(?=})")  #expression to extract cdmSites from {listid:cdmSites}
+$ListsToCreate =  @{}
+
+Get-ChildItem -Path $TemplatePath | ForEach-Object{
+    [XML]$Template = Get-Content -Path $_.PSPath
+    $BaseList = $Template.Provisioning.Templates.ProvisioningTemplate.Lists.ListInstance
+    $DependentList = ($BaseList.Fields.Field | Where-Object {$_.Type -eq 'Lookup'}).List
+    if ($DependentList -eq $null){ $ListsToCreate[$BaseList.Title] = @()}
+    $ListsToCreate[$BaseList.Title] = @($DependentList | ForEach-Object {$Regex.Match($_).Value})
+}
+
+#Created a sorted list of lists to create
+$ListsInOrder = Get-TopologicalSort $ListsToCreate
+$ListOrder = $ListsInOrder | Where-Object {$_} #remove empty strings
+
+#Create all lists from templates
+Foreach ($item in $ListOrder){
+    
+    $TempalteName = "$($TemplatePath)$($item)Template.xml"
+    Write-Host "Creating $($item)..."
+    Apply-PnPProvisioningTemplate -Path $TempalteName -ErrorAction Continue -Verbose
+    Write-Host "Done`n"
+} 
+
+# Add indexes to: cdmHazards list '& cdmStages list 'Title' field
+
+$fieldsToIndex = 'cdmCurrentStatus','cdmHazardOwner','cdmResidualRiskScore','cdmSite','Created By','Modified','Modified By','cdmStage'
+$fieldsToIndex | ForEach-Object {Set-PnPField -List 'cdmHazards' -Identity $_ -Values @{Indexed=$true}}
+
+Set-PnPField -List 'cdmStages' -Identity 'Title' -Values @{Indexed=$true}
+
+#---------------------------------------
+# POPULATE LISTS WITH DEFAULT VALUES FROM EXCEL SPREADSHEET
+#---------------------------------------
+
+# Optional - Can be done in SharePoint
+
+#---------------------------------------
+# Get Lists to populate and sort them
+#---------------------------------------
+
+#Get lists from Excel file tab names
+$Sheets = Get-ExcelSheetInfo -Path $ExcelListPath
+              
+# Add a property to stor the lists which need to be populated before each list
+$Sheets | Add-Member -MemberType NoteProperty -Name "Dependencies" -Value @()
+
+#Get dependency list
+
+foreach ($lst in $Sheets){
+    
+    #Write-Host "List $($lst.Name)\n"
+    $cdmLookupFields = Get-PnPField -List $lst.Name | Where-Object {$_.Title -like 'cdm*' -and $_.FieldTypeKind -eq 'Lookup' }
+    foreach ($fld in $cdmLookupFields){
+        #Write-Host "Lookup field $($fld.Title)"
+        $depList = Get-PnPList -Identity $fld.LookupList
+        #Write-host "Dependant List $($depList.Title)"
+        if($lst.Dependencies -notcontains $depList.Title){
+            $lst.Dependencies += $depList.Title
+        }
+    }
+}
+
+#convert dependecy list to hashtable
+$ListHash = @{}
+foreach ($l in $Sheets){
+    $ListHash["$($l.Name)"]=$l.Dependencies
+}
+
+#Sort list based on dependancies
+$SortedList = Get-TopologicalSort $ListHash
+
+$SortedList2 = @()
+foreach ($ts in $SortedList){
+    if($Sheets.Name -contains $ts){
+        $SortedList2 += $ts
+    }
+}
+
+#---------------------------------------
+# Populate lists from Excel file
+#---------------------------------------
+        
+foreach ($sheet in $SortedList2){
+    
+    #Import Sheet
+    $ImportedSheet = Import-Excel -Path $ExcelListPath -DataOnly -WorksheetName $sheet -ErrorAction SilentlyContinue
+    
+    #Find lookup fields and replace values with ID of item looked up
+    $LookupFields = Get-PnPField -List $sheet | Where-Object {$_.Title -like 'cdm*' -and $_.FieldTypeKind -eq 'Lookup' }
+    foreach ($lkp in $LookupFields){
+        $list = Get-PnPList -Identity $lkp.LookupList
+        $Lookups = Get-PnPListItem $list
+        # loop through imported row. Replace lookup value with item ID.
+        foreach ($r in $ImportedSheet){
+            $r."$($lkp.InternalName)"= ($Lookups | Where-Object {$_.FieldValues.Title -eq $r."$($lkp.InternalName)"}).Id
+        }
+    }
+
+    #Import all rows
+    
+              
+    #Loop through Rows
+    foreach ($row in $ImportedSheet){     
+       
+        #Add list item
+        
+        $ItemHash = $row | ConvertTo-HashtableFromPsCustomObject
+        $NewItem = Add-PnPListItem -List "$($sheet)" -ContentType 'Item' -Values $ItemHash -Verbose
+        }
+        if($ImportSheet.Count -gt 0){
+            Write-Host "`n $($sheet) List populated`n" -ForegroundColor Green
+        }
+}
+
+#---------------------------------------
+# Add a link to the SafetIbase dashboard to the rhs navigation menu
+#---------------------------------------
+
+Add-PnPNavigationNode -Location QuickLaunch -Title 'SafetIbase' -Url 'SiteAssets/pages/3.0/dashboard.aspx' -ErrorAction Continue
+
+#---------------------------------------
+# END OF SCRIPT
+#---------------------------------------
---
+++ README.md
@@ -0,0 +1,59 @@
+# SafetIbase
+SafetIbase is an award winning system to improve the identification, management and communication of health and safety hazards for construction projects. Funded by i3P, it is an open source solution for the industry.
+
+#####################################################
+
+Deploy Safetibase on a SharePoint Site
+-----------------------------------------------------
+Requirements:
+- Windows 10
+- SharePoint Project Site (sites from other templates might not work)
+- Owner rights on your SharePoint site
+-----------------------------------------------------
+To install SafetIbase on a SharePoint, follow the steps below:
+- Download these files to your C drive (downloading the whole repo from Github using the green 'Code' button on the top right, the select 'Download ZIP')
+- Unblock the zip file (right click > properties > check the 'Unblock' checkbox)
+- Open PowerShell (PowerShell ISE is the easiest option)
+- Open 'Manual Instal.ps1'
+- In the User input section, change the Url to the url of your SharePoint site
+- Go to your SharePoint site > 'Site Contents' > 'Site Assets'
+- Drag and drop the content of the 'Site Asset' folder from the install files
+- Run the script
+- [Optional] Fill in the 'SafetIbaseListsContent - UserInput Template.xlsx' spreadsheet with your project specifc data. Delete tabs your are not updating
+- [Optional] In PowerShell open 'Add User Inputs.ps1' and in the 'User Input' section, input the url of your site
+- [Optional] Run the script. This will populate the SharePoint lists with the input you provide in the spreadsheet above
+- To find your SafetIbase page, go to 'Site Contents' > 'Site Assets' > 'pages' > '3.0' > 'dashboard.aspx'
+-----------------------------------------------------
+CHANGE LOG:
+
+06/10/20
+  -Cleaned up tw.txt & rams.txt
+  -Corrected the user roles being autoamticaly populated. Remove 'System admin', 'RAG admin', 'RAMS admin', 'TW admin' as they are not used.
+  -Modified code to deal better with large number of hazards (up to 5000 hazards per tile in the SafetIbase dashboard). Includes adding extra indices to cdmHazards
+  -Added home button and removed the link from the SafetIbase icon (unstable)
+  -Removed requirement to run PowerShell as Administrator (install modules in user scope and change execution policy only in user scope).
+  -Added Update_SafetIbase.ps1 This can be used to updae SafetIbase on a SP site where it is already isntalled. It will upload the latest relevant files from Github and add the required indices
+
+15/11/19
+  -Incorporated changes to new.hazard.form.html from evanpreslar-bentley
+
+30/10/19
+  -Added indexes to 'cdmStage' & 'Modified' fields (cdmHazards list) and 'Title' field (cdmStages)
+
+07/10/19
+  -Changed multiple files to integration with 'Bentley Design Review Risk Management'. At the moment requires manual indexing of the 'Modified' filed in cdmHazard
+
+13/08/2019
+  -Corrected typo in 'Manual Instal.ps1' l.110 variable $ListsToCreate
+
+09/08/2019
+  -Grouped all functions in SafetIbaseFunctions.psm1
+  -Modified 'Manual Instal.ps1' to create a link to the SafetIbase dashboard in the SP site quick links
+  -Modified 'Manual Instal.ps1'to automatically sort lists to create based on lookup column dependencies
+  -Modified 'Manual Instal.ps1'& 'Add User Inputs.ps1' to correct a bug when adding items with lookup columns
+  -Modified 'Manual Instal.ps1'& 'Add User Inputs.ps1' to sort lists to update based on lookup column dependencies
+
+01/08/2019
+  -Rationalised the 'Manual Instal.ps1' script
+  -Modified 'user.dashboard.layout.1.css' to correct margins in hazard list on MM SP sites
+  -Site Assets updated to version received from CVB in June 2019
